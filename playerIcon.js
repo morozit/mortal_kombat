ccb238c1e3afc945fc45b5a2954f4fb42d0806fd
Roman <itmorozr@gmail.com>
2021-04-18 05:35:49 +0300

    Create playerIcon.js

---
+++ playerIcon.js
@@ -0,0 +1,8 @@
+export const playerIcon = {
+  scorpion: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+  kitana: 'http://reactmarathon-api.herokuapp.com/assets/kitana.gif',
+  liukang: 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif',
+  sonya: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
+  subzero: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
+  orco: 'https://www.fightersgeneration.com/nx/chars/kintaro-arcadestance.gif',
+};
