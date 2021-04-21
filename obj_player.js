3ab7534e9c517437f4a10ef37cbefa7b7e7815c1
Roman <itmorozr@gmail.com>
2021-04-21 03:18:18 +0300

    Update obj_player.js

--- obj_player.js
+++ obj_player.js
@@ -1,39 +1,87 @@
 import { playerIcon } from "./playerIcon.js";
-import { changeHP, renderHP, elHP } from "./object_method.js";
+// import { changeHP, renderHP, elHP } from "./object_method.js";
+// import { changeHP, renderHP, elHP } from "./main.js";
 
 export const obj_PLAYER = {
   player: 0,
-  name: '',
+  name: 'liukang',
   hp: 100,
-  img: playerIcon.name,
+  // color: {
+    // colorText: '#5E00FF',
+    // colorName: `<span style='color: ${obj_PLAYER.color.colorText}; font-weight: bold;'>-${obj_PLAYER.name}</span>`,
+    // colorHp: `<span style='color: ${obj_PLAYER.color.colorText}; font-weight: bold;'>-${obj_PLAYER.hp}</span>`,
+  // },
+  img: playerIcon.liukang,
   changeHP,
   renderHP,
   elHP,
   attack: 0,
 };
 
+
 export const obj_PLAYER_1 = {
   player: 1,
-  name: 'sonya',
+  name: 'SONYA',
   hp: 100,
   img: playerIcon.sonya,
+  kick: 0,
+  color: '#42DDF5',
   changeHP,
   renderHP,
   elHP,
-  kick: 0,
 };
 
 export const obj_PLAYER_2 = {
   player: 2,
-  name: 'orco',
+  name: 'ORCO',
   hp: 100,
   img: playerIcon.orco,
-  weapon: ['трезубец'],
+  kick: 0,
+  color: '#FFFF00',
   changeHP,
   renderHP,
   elHP,
-  kick: 0,
 };
 
+
+// деструктуризация
+const {
+  player: firstPlayer, 
+  name: nameFirstPlayer, 
+  hp: hpFirstPlayer,
+  img: imgFirstPlayer,
+  kick: kickFirstPlayer,
+  color: colorFirstPlayer,} = obj_PLAYER_1;
+
+const {
+  player: secondPlayer, 
+  name: namesecondPlayer, 
+  hp: hpsecondPlayer,
+  img: imgsecondPlayer,
+  kick: kicksecondPlayer,
+  color: colorsecondPlayer,} = obj_PLAYER_2;
+
+
+// прототип Object
 export const obj_PLAYER_3 = Object.create(obj_PLAYER);
   
+// !!!!  HP ---------------------------
+export function changeHP(lostLife) {
+  this.hp -= lostLife;
+  if (this.hp <= 0) {
+    this.hp = 0;
+  }
+
+  return this.hp;
+}
+
+export function elHP() {
+  let $playerLife = document.querySelector(`.player${this.player} .life`);
+  return $playerLife;
+} 
+
+export function renderHP() {
+  this.elHP().style.width = `${this.hp}%`
+  console.log();
+  return this.name
+}
