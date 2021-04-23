11ae9f0bc1aa363c7143d6b23e86dc35177e4461
Roman <itmorozr@gmail.com>
2021-04-23 23:31:09 +0300

    delete

--- UserOne.js
+++
@@ -1,5 +0,0 @@
-class UserOne extends Player {
-  constructor(player, name, hp, img, kick, color, changeHP, renderHP, elHP) {
-    super(player, name, hp, img, kick, color, changeHP, renderHP, elHP);
-  }
-}
--- UserTwo.js
+++
@@ -1,5 +0,0 @@
-class UserTwo extends Player {
-  constructor(player, name, hp, img, kick, color, changeHP, renderHP, elHP) {
-    super(player, name, hp, img, kick, color, changeHP, renderHP, elHP);
-  }
-}
