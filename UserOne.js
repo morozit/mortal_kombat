2898075947f9d45e42ecb19da316d3eebd74fe31
Roman <65686226+morozit@users.noreply.github.com>
2021-04-23 16:00:50 +0300

    Create UserOne.js

---
+++ UserOne.js
@@ -0,0 +1,5 @@
+class UserOne extends Player {
+  constructor(player, name, hp, img, kick, color, changeHP, renderHP, elHP) {
+    super(player, name, hp, img, kick, color, changeHP, renderHP, elHP);
+  }
+}
